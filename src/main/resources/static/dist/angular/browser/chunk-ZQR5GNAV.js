31a71fd0299a959e3e14b2857ef50b1b88c9efb5
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-07 19:41:22 -0300

    Updated addrress for vm

--- src/main/resources/static/dist/angular/browser/chunk-ZQR5GNAV.js
+++
@@ -1,1 +0,0 @@
-import{k as a}from"./chunk-SEADAN7C.js";export{a as HomeComponent};
--- src/main/resources/static/dist/angular/server/main.server.mjs
+++ src/main/resources/static/dist/angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BE6BXJQS.mjs";import"./chunk-KQ6U7NCT.mjs";import"./chunk-4U3JSVZE.mjs";import"./chunk-57ATWE5R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YJRT3EXX.mjs";import"./chunk-KQ6U7NCT.mjs";import"./chunk-TQ5RDNTO.mjs";import"./chunk-57ATWE5R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
