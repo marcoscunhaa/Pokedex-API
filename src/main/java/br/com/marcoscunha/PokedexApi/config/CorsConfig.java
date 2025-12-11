74ba25abe961c49024746877b2d2870bbce73331
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-11 09:57:49 -0300

    Optimized for DuckDNS domain

--- src/main/java/br/com/marcoscunha/PokedexApi/config/CorsConfig.java
+++ src/main/java/br/com/marcoscunha/PokedexApi/config/CorsConfig.java
@@ -16,9 +16,9 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:4200",
-                                "https://pokedex-sigma-plum.vercel.app",
+                                "https://pokedex-sigma-mocha.vercel.app",
                                 "https://pokedex-git-main-marcos-projects-c01daa0e.vercel.app",
-                                "https://pokedex-5bxg2jx36-marcos-projects-c01daa0e.vercel.app",
+                                "https://pokedex-ew5bpqoy4-marcos-projects-c01daa0e.vercel.app",
                                 "https://pokedex-api.duckdns.org" // seu novo domínio
                         )
                         .allowedMethods("*")
