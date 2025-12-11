82d767fc3f820c4af0ad812a083a25139dfbc63c
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-11 11:13:12 -0300

    Optimized for DuckDNS domain

---
+++ src/main/java/br/com/marcoscunha/PokedexApi/controller/FrontendController.java
@@ -0,0 +1,12 @@
+package br.com.marcoscunha.PokedexApi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+    @GetMapping("/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
