2063f661b1956d7f6008097df893d3972fdca0c8
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-11 07:55:31 -0300

    Removed frontend

--- src/main/resources/static/chunk-FM7MCWUA.js
+++
@@ -1,1 +0,0 @@
-import{k as a}from"./chunk-HJGY6WC7.js";export{a as HomeComponent};
--- src/main/resources/static/dist/angular/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-YN4JZAPR.mjs";import"./chunk-KQ6U7NCT.mjs";import"./chunk-25NZGACF.mjs";import"./chunk-57ATWE5R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
