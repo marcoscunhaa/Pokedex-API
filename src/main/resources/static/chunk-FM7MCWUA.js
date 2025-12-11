e31f9713d19048814379b4bea4abdb66b329e5e4
Marcos <marcos.cunha1025@gmail.com>
2025-12-10 22:12:09 -0300

    Updated route of server

---
+++ src/main/resources/static/chunk-FM7MCWUA.js
@@ -0,0 +1,1 @@
+import{k as a}from"./chunk-HJGY6WC7.js";export{a as HomeComponent};
--- src/main/resources/static/dist/angular/server/main.server.mjs
+++ src/main/resources/static/dist/angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6UQG5ZLY.mjs";import"./chunk-KQ6U7NCT.mjs";import"./chunk-LWFMXYCH.mjs";import"./chunk-57ATWE5R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-YN4JZAPR.mjs";import"./chunk-KQ6U7NCT.mjs";import"./chunk-25NZGACF.mjs";import"./chunk-57ATWE5R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
