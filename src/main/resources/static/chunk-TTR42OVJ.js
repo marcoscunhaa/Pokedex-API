6117e7d8805d72822be8f8c0982280c94b371801
Marcos Cunha <marcos.cunha1025@gmail.com>
2025-12-12 15:48:49 -0300

    Android version

--- src/main/resources/static/chunk-TTR42OVJ.js
+++
@@ -1,1 +0,0 @@
-import{k as a}from"./chunk-B3IELIXK.js";export{a as HomeComponent};
--- src/main/resources/static/dist/angular/server/main.server.mjs
+++ src/main/resources/static/dist/angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-PB5M7GYS.mjs";import"./chunk-BMFXI2C5.mjs";import"./chunk-ABC2RGFP.mjs";import"./chunk-SG4KX3VQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RB5OGXL4.mjs";import"./chunk-BMFXI2C5.mjs";import"./chunk-RAFTN2IP.mjs";import"./chunk-SG4KX3VQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
